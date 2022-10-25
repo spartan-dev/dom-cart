f9e87ec6c05e5fe5a377fd454997630acbb6c0bd
Salvador Emmanuel <s.e.jugr@gmail.com>
2022-10-25 02:05:41 -0500

    chore: add comments to code :bug:

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ const subtotalEl = document.querySelector(".subtotal");
 const totalItemsInCartEl = document.querySelector(".total-items-in-cart");
 
 // RENDER PRODUCTS
-function renderProdcuts() {
+
+/**
+ * @function renderProducts
+ * @params no recibe parametros solo se ejecuta para crear dinamicamente el div que contiene la card
+ */
+function renderProducts() {
   products.forEach((product) => {
+    /**
+     * recuerden el template extraido del html recibe props para ser dinamico
+     * */
     productsEl.innerHTML += `
             <div class="item">
                 <div class="item-container">
@@ -31,20 +39,35 @@ function renderProdcuts() {
         `;
   });
 }
-renderProdcuts();
+// !importante checar el metodo en el div como onclick = a la funcion addCart(productoId)
+renderProducts()
 
 // cart array
+/**
+ * Esta variable cart nos deja extraer de la memoria local del browser cierta mini info para consultar despues
+ * localStorage
+ * lleva un or con el objeto vacio o previamente llenado
+ * @method getItem()
+ */
+
 let cart = JSON.parse(localStorage.getItem("CART")) || [];
 updateCart();
+//hacemos una llamada a la updateCart funcion que vive mas abajo solita y triste :sad:
 
-// ADD TO CART
+// agregar al carrito
+/**
+ * @function addToCart
+ * @param id que viene del template del producto le envia el id al momento del click
+ */
+
 function addToCart(id) {
-  // check if prodcut already exist in cart
+  // checamos su el producto ya esta en el carrito
   if (cart.some((item) => item.id === id)) {
+    //si ya esta le suma un + a el que ya existe
     changeNumberOfUnits("plus", id);
   } else {
+    //si no esta le empuja un nuevo objeto al arreglo de cart
     const item = products.find((product) => product.id === id);
-
     cart.push({
       ...item,
       numberOfUnits: 1,
@@ -56,32 +79,49 @@ function addToCart(id) {
 
 // update cart
 function updateCart() {
+  //esta llama a el renderCart al momento de refrescar como re-llamada
   renderCartItems();
+  //al igual qye esra que revisa el subtotal
   renderSubtotal();
 
   // save cart to local storage
+  //este hace el guardado del elemento en la memoria que vimos arriba se acuerdan ??
+  //con uno es set del item y el otro es get del item
+  //el metodo de JSON.stringify lo pasa a solo texto solo asi se guarda
   localStorage.setItem("CART", JSON.stringify(cart));
 }
 
 // calculate and render subtotal
+/**
+* @function renderSubtotal
+ * tenmos dos variables que son para almacenar el total y el subtotal
+* */
 function renderSubtotal() {
-  let totalPrice = 0,
-    totalItems = 0;
 
+  let totalPrice = 0
+  let  totalItems = 0
+ //repasamos el arreglo del carro previamente llenado con el push del addToCart()
   cart.forEach((item) => {
     totalPrice += item.price * item.numberOfUnits;
     totalItems += item.numberOfUnits;
   });
-
+  //llenamos el subtotal que sujeta el valor de html
   subtotalEl.innerHTML = `Subtotal (${totalItems} items): $${totalPrice.toFixed(
     2
   )}`;
+  //llena el lugar del total que sujeta con el total del html
   totalItemsInCartEl.innerHTML = totalItems;
 }
 
 // render cart items
+/**
+ * @function renderCartItems
+ * @params no lleva ningun parametro
+* */
 function renderCartItems() {
+  // aca aplicamos el mismo tratamiento que le dimos a los productos
   cartItemsEl.innerHTML = ""; // clear cart element
+  //y por cada elemento hacemos un nuevo template que llena el espacio de la lista del carrito
   cart.forEach((item) => {
     cartItemsEl.innerHTML += `
         <div class="cart-item">
@@ -101,32 +141,40 @@ function renderCartItems() {
       `;
   });
 }
-
+//!importante el onclick de los botones es quien se encarga de de sumar con su funcion de changeNumberOfUnits(simbolo, id)
 // remove item from cart
 function removeItemFromCart(id) {
+  //con este se remueve del arreglo previamente llenado
   cart = cart.filter((item) => item.id !== id);
-
+  // y actualizamos :arrow_down:
   updateCart();
 }
 
 // change number of units for an item
+/**
+ * @function changeNumberOfUnits
+ * @param(action) suma o resta
+ * @param(id) id de el producto
+ * */
 function changeNumberOfUnits(action, id) {
+  //hacemos un arreglo copia del que ya tenemos
   cart = cart.map((item) => {
     let numberOfUnits = item.numberOfUnits;
-
+   //validamos si es igual el id que llega del que esta en la iteracion
     if (item.id === id) {
+      //disminuiosmos o aumentamos las unidades
       if (action === "minus" && numberOfUnits > 1) {
         numberOfUnits--;
       } else if (action === "plus" && numberOfUnits < item.instock) {
         numberOfUnits++;
       }
     }
-
+ //regresamos el valor con un objeto el item y el numero de unidades
     return {
       ...item,
       numberOfUnits,
     };
   });
-
+// y que cren ? llamamos al update para cambiar de nuevo el carro
   updateCart();
 }
